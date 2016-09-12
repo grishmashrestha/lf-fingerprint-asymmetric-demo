29f5d0f95131d9520c89a0bc1331dff80d963629
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-09-12 13:07:12 +0545

    Create fingerprint reader without fingerprintdialog

--- app/src/main/java/np/com/grishma/fingerprintdemo/MainActivity.java
+++ app/src/main/java/np/com/grishma/fingerprintdemo/MainActivity.java
@@ -1,7 +1,13 @@
 package np.com.grishma.fingerprintdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import np.com.grishma.fingerprintdemo.withoutdialog.FingerprintWithoutDialogActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.button_with_dialog, R.id.button_without_dialog})
+    public void setOnClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.button_with_dialog:
+                intent = new Intent(this, FingerprintWithoutDialogActivity.class);
+                break;
+
+            case R.id.button_without_dialog:
+                intent = new Intent(this, FingerprintWithoutDialogActivity.class);
+                break;
+        }
+        startActivity(intent);
+
     }
 }
